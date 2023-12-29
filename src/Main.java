2de548dd4cba9282eea1e61eef06ed9c96d66bc4
eunddung <x72845221@gmail.com>
2023-12-29 13:29:03 +0900

    게시물 목록 기능 구현

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("== 프로그램 시작 == ");
-
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
+		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 			System.out.print("명령어 > ");
 			String cmd = sc.nextLine().trim();
-
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력하세요");
 				continue;
 			}
-
 			if (cmd.equals("exit")) {
 				break;
 			}
@@ -28,18 +28,64 @@ public class Main {
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
 
+				Article article = new Article(id, title, body);
+				articles.add(article);
 //				System.out.println(title + " / " + body);
 
 				System.out.printf("%d번 글이 생성 되었습니다.\n", id);
 				lastArticleId++;
 			} else if (cmd.equals("article list")) {
 				System.out.println("==게시글 목록==");
+				if (articles.size() == 0) {
+					System.out.println("아무것도 없어");
+				} else {
+					System.out.println("  번호  /  제목  ");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("  %4d  /   %s  \n", article.getId(), article.getTitle());
+					}
+				}
 			} else {
 				System.out.println("사용할 수 없는 명령어입니다");
 			}
 		}
-
 		System.out.println("== 프로그램 끝 == ");
 		sc.close();
+	}
+}
+
+class Article {
+	private int id;
+	private String title;
+	private String body;
+
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
 	}
 }
