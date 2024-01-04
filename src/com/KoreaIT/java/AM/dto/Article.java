5053819f65f7026f0ab14c904c857f03d492cdd2
eunddung <x72845221@gmail.com>
2024-01-04 11:00:08 +0900

    수정 삭제시 로그인 해야 가능하도록 작성시 작성자에 대한 정보가 같이 남도록

--- src/com/KoreaIT/java/AM/dto/Article.java
+++ src/com/KoreaIT/java/AM/dto/Article.java
@@ -1,71 +1,68 @@
 package com.KoreaIT.java.AM.dto;
-
 public class Article extends Dto {
-
 	private String updateDate;
-
 	private String title;
 	private String body;
 
+	private int memberId;
+
 	private int hit;
 
-	public Article(int id, String regDate, String updateDate, String title, String body) {
-		this(id, regDate, updateDate, title, body, 0);
+	public Article(int id, String regDate, String updateDate, int memberId, String title, String body) {
+		this(id, regDate, updateDate, memberId, title, body, 0);
 	}
 
-	public Article(int id, String regDate, String updateDate, String title, String body, int hit) {
+	public Article(int id, String regDate, String updateDate, int memberId, String title, String body, int hit) {
 		this.id = id;
 		this.regDate = regDate;
 		this.updateDate = updateDate;
+		this.memberId = memberId;
 		this.title = title;
 		this.body = body;
 		this.hit = hit;
 	}
 
+	public int getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getRegDate() {
 		return regDate;
 	}
-
 	public void setRegDate(String regDate) {
 		this.regDate = regDate;
 	}
-
 	public String getUpdateDate() {
 		return updateDate;
 	}
-
 	public void setUpdateDate(String updateDate) {
 		this.updateDate = updateDate;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getBody() {
 		return body;
 	}
-
 	public void setBody(String body) {
 		this.body = body;
 	}
-
 	public int getHit() {
 		return hit;
 	}
-
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
