7eb5d2f452ce8b4d4a6e74cbb1f77a66049733d7
eunddung <x72845221@gmail.com>
2024-01-04 11:39:16 +0900

    로그인 로그아웃 체크카드를 App에서 처리 컨트롤러 내부에선 삭제

--- src/com/KoreaIT/java/AM/App.java
+++ src/com/KoreaIT/java/AM/App.java
@@ -12,12 +12,9 @@ public class App {
 		MemberController memberController = new MemberController(sc);
 		ArticleController articleController = new ArticleController(sc);
 		Controller controller = null;
-
 		articleController.makeTestData();
 		memberController.makeTestData();
-
 		while (true) {
-			 
 			System.out.print("명령어 > ");
 			String cmd = sc.nextLine().trim();
 			if (cmd.length() == 0) {
@@ -42,8 +39,34 @@ public class App {
 				System.out.println("사용할 수 없는 명령어입니다3");
 				continue;
 			}
+
+			String forLoginCheck = controllerName + "/" + actionMethodName;
+
+			switch (forLoginCheck) {
+			case "article/write":
+			case "article/modify":
+			case "article/delete":
+			case "member/logout":
+				if (Controller.isLogined() == false) {
+					System.out.println("로그인 하고 이용해");
+					continue;
+				}
+				break;
+			}
+
+			switch (forLoginCheck) {
+			case "member/login":
+			case "member/join":
+				if (Controller.isLogined()) {
+					System.out.println("로그아웃 하고 이용해");
+					continue;
+				}
+				break;
+			}
+
 			controller.doAction(actionMethodName, cmd);
 		}
+
 		System.out.println("== 프로그램 끝 == ");
 		sc.close();
 	}
--- src/com/KoreaIT/java/AM/controller/MemberController.java
+++ src/com/KoreaIT/java/AM/controller/MemberController.java
@@ -1,20 +1,22 @@
 package com.KoreaIT.java.AM.controller;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import com.KoreaIT.java.AM.dto.Member;
 import com.KoreaIT.java.AM.util.Util;
+
 public class MemberController extends Controller {
 	private List<Member> members;
 	private Scanner sc;
 	private String cmd;
-
 	int lastMemberId = 3;
 
 	public MemberController(Scanner sc) {
 		this.members = new ArrayList<>();
 		this.sc = sc;
 	}
+
 	public void doAction(String actionMethodName, String cmd) {
 		this.cmd = cmd;
 		switch (actionMethodName) {
@@ -22,17 +24,9 @@ public class MemberController extends Controller {
 			doJoin();
 			break;
 		case "login":
-			if (isLogined()) {
-				System.out.println("이미 로그인 상태야");
-				return;
-			}
 			doLogin();
 			break;
 		case "logout":
-			if (!isLogined()) {
-				System.out.println("이미 로그아웃 상태야");
-				return;
-			}
 			doLogout();
 			break;
 		default:
@@ -63,9 +57,9 @@ public class MemberController extends Controller {
 
 	private void doLogout() {
 		loginedMember = null;
-
 		System.out.printf("로그아웃 성공!\n");
 	}
+
 	private void doJoin() {
 		System.out.println("==회원 가입==");
 		int id = lastMemberId + 1;
@@ -99,6 +93,7 @@ public class MemberController extends Controller {
 		System.out.printf("%d번 회원이 가입 되었습니다. %s님 환영합니다.\n", id, name);
 		lastMemberId++;
 	}
+
 	private Member getMemberByLoginId(String loginId) {
 		for (Member member : members) {
 			if (member.getLoginId().equals(loginId)) {
@@ -107,6 +102,7 @@ public class MemberController extends Controller {
 		}
 		return null;
 	}
+
 	private boolean isJoinableLoginId(String loginId) {
 		for (Member member : members) {
 			if (member.getLoginId().equals(loginId)) {
@@ -115,6 +111,7 @@ public class MemberController extends Controller {
 		}
 		return true;
 	}
+
 	public void makeTestData() {
 		System.out.println("테스트를 위한 회원 데이터를 생성합니다.");
 		members.add(new Member(1, Util.getNowDate_TimeStr(), "admin", "admin", "관리자"));
